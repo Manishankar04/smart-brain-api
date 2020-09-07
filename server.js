ead463c12ca47ad2b6c9d71b5769ea796c8949b0
Manishankar04 <es.maniobu@gmail.com>
2020-09-07 17:18:25 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image =require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-objective-64282',
     user : 'postgres',
     password : 'node',
     database : 'smart-brain'
